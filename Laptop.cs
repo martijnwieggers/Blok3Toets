93fc6498740e343f3ebf4464ddebfc2f4e3fd028
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2024-01-10 11:10:33 +0100

    commentaar aangemaakt

--- Laptop.cs
+++ Laptop.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 public class Laptop
 {
+    //Publieke eigenschappen komen hier onder:
 
+
+    //Constructor met alle eigenschappen komt hieronder:
+
+    //Methode ToonInfo() komt hieronder:
+
+    //Methode VraagInfo() komt hieronder:
+
+    //
 
 
     //Default Constructor, laten staan
--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 ﻿//Lijst met Laptop objecten, laten staan
 List<Laptop> Laptops = new List<Laptop>();
 
+//Maak 3 laptops aan met constructor die alle eigenschappen direct vult en voeg deze toe aan lijst Laptops
 
+//Maak 1 laptop aan met de constructor zonder parameters. 
+
+//Zorg dat de informatie wordt gevraagd door VraagInfo() aan te roepen
+
+//Voeg ook deze laptop toe aan de lijst Laptops
 
 
 foreach (Laptop laptop in Laptops)
 {
-
+    //Roep voor iedere laptop in de lijst ToonInfo() aan.
 }
